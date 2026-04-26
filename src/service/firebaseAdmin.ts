be90bc0eeb9d404fa6847019dd9968a989c62c65
Marcel <Marcelyap31@gmail.com>
2026-04-26 18:27:47 +0800

    added firebase admin package and fixed import error

--- src/service/firebaseAdmin.ts
+++ src/service/firebaseAdmin.ts
@@ -1,4 +1,4 @@
-const admin = require("firebase-admin");
+import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -6,7 +6,7 @@ if (!admin.apps.length) {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+    } as admin.ServiceAccount),
   });
 }
 
